924624a5bc4265f12e092b2a938ff297d0905399
dExBrunno <brunnoromanato@hotmail.com>
2023-08-17 15:00:08 -0300

    bloco noticias e veja como é facil

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,8 +10,18 @@ import Img1 from '../static/images/home/img2.png';
 import Image from "next/image";
 import Hits1Img1 from '../static/images/img1_hits1.png';
 import Hits2Img2 from '../static/images/img1_hits2.png';
+import imgNovidades1 from '../static/images/home/novidades1.png'
+import imgNovidades2 from '../static/images/home/novidades2.png'
+import imgNovidades3 from '../static/images/home/novidades3.png'
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import { Swiper, SwiperSlide } from "swiper/react";
+
+
 
 const Home: NextPage = () => {
+
+
     return (
         <MainTemplate>
             <Head>
@@ -58,77 +68,128 @@ const Home: NextPage = () => {
                 <div className="pt-32">
                     <h2 className="text-5xl text-black font-light text-center mb-8">Veja como é fácil</h2>
                     <p className="text-grey md:text-black text-base md:text-lg font-light font-ubuntu mb-8 w-[808px] mx-auto text-center">
-                        Se você tem um comércio ou é atacadista, prestador de serviços, construtura ou pequena indústria, você precisa testar essa solução pra manter suas contas em dia, agilizar processos
+                        Se você tem um <strong>comércio ou é atacadista, prestador de serviços, construtura ou pequena indústria</strong>, você precisa testar essa solução pra manter suas contas em dia, agilizar processos
                         e acelerar o crescimento do seu negócio.
                     </p>
                     <div className="pt-16">
-                        <div className="flex items-center justify-center gap-8">
-                            <div className="md:w-[283px] md:h-[283px] border rounded-full bg-cold-white flex flex-col justify-center items-center">
-                                <div className="mb-4">
-                                    <Phone />
+                        <div className="flex items-center justify-center gap-8 max-xl:w-[50%] mx-auto max-sm:w-[283px]">
+                        <Swiper slidesPerView={4} initialSlide={1}
+                            breakpoints={{
+                                320: {
+                                    slidesPerView: 1,
+                                    spaceBetween: 20,
+                                },
+                                768: {
+                                    slidesPerView: 2,
+                                    spaceBetween: 20,
+                                },
+                                1280: {
+                                    slidesPerView: 4,
+                                }
+                              }}
+                        >
+                            <SwiperSlide>
+                                <div className="w-[283px] h-[283px] border mr-6 rounded-full bg-cold-white flex flex-col justify-center items-center">
+                                    <div className="mb-4">
+                                        <Phone />
+                                    </div>
+                                    <h3 className="text-black font-medium text-2xl mb-2">Controle</h3>
+                                    <p className="w-[188px] text-center mx-auto">Controle suas despesas em tempo real</p>
                                 </div>
-                                <h3 className="text-black font-medium text-2xl mb-2">Controle</h3>
-                                <p className="w-[188px] text-center mx-auto">Controle suas despesas em tempo real</p>
-                            </div>
-                            <div className="md:w-[283px] md:h-[283px] border rounded-full bg-cold-white flex flex-col justify-center items-center">
-                                <div className="mb-4">
-                                    <Phone />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <div className="w-[283px] h-[283px] border mr-6 rounded-full bg-cold-white flex flex-col justify-center items-center">
+                                    <div className="mb-4">
+                                        <Gas />
+                                    </div>
+                                    <h3 className="text-black font-medium text-2xl mb-2">Controle</h3>
+                                    <p className="w-[188px] text-center mx-auto">Controle suas despesas em tempo real</p>
                                 </div>
-                                <h3 className="text-black font-medium text-2xl mb-2">Controle</h3>
-                                <p className="w-[188px] text-center mx-auto">Controle suas despesas em tempo real</p>
-                            </div>
-                            <div className="md:w-[283px] md:h-[283px] border rounded-full bg-cold-white flex flex-col justify-center items-center">
-                                <div className="mb-4">
-                                    <Phone />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <div className="w-[283px] h-[283px] border mr-6 rounded-full bg-cold-white flex flex-col justify-center items-center">
+                                    <div className="mb-4">
+                                        <Clock />
+                                    </div>
+                                    <h3 className="text-black font-medium text-2xl mb-2">Controle</h3>
+                                    <p className="w-[188px] text-center mx-auto">Controle suas despesas em tempo real</p>
                                 </div>
-                                <h3 className="text-black font-medium text-2xl mb-2">Controle</h3>
-                                <p className="w-[188px] text-center mx-auto">Controle suas despesas em tempo real</p>
-                            </div>
-                            <div className="md:w-[283px] md:h-[283px] border rounded-full bg-cold-white flex flex-col justify-center items-center">
-                                <div className="mb-4">
-                                    <Phone />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <div className="w-[283px] h-[283px] border mr-6 rounded-full bg-cold-white flex flex-col justify-center items-center">
+                                    <div className="mb-4">
+                                        <Note />
+                                    </div>
+                                    <h3 className="text-black font-medium text-2xl mb-2">Controle</h3>
+                                    <p className="w-[188px] text-center mx-auto">Controle suas despesas em tempo real</p>
                                 </div>
-                                <h3 className="text-black font-medium text-2xl mb-2">Controle</h3>
-                                <p className="w-[188px] text-center mx-auto">Controle suas despesas em tempo real</p>
-                            </div>
+                            </SwiperSlide>
+                        </Swiper>
                         </div>
                     </div>
                 </div>
                 <div className="pt-32">
                     <h2 className="text-5xl text-black font-light text-center mb-16">Veja as novidades</h2>
-                    <div className="flex gap-8 items-center justify-center pb-16">
-                        <div className="w-[368px] h-[440px]">
-                            <div className="h-[235px] bg-grey rounded-t-3xl"></div>
-                            <div className="h-[205px] bg-cold-white rounded-b-3xl p-10">
-                                <span className="font-ubuntu text-grey block mb-6">19 Jun 2023</span>
-                                <h3 className="text-black font-medium text-base mb-4">Cartão de combustível: o que é, como funciona  e qual o melhor?</h3>
-                                <div className="-ml-[20px]">
-                                    <Button type="readMore" href="/">Leia mais</Button>
+                    <div className="m-auto md:max-w-[67%] xl:max-[80%]: max-md:w-[368px]">
+                        <Swiper slidesPerView={3} initialSlide={1} spaceBetween={30}
+                            breakpoints={{
+                                320: {
+                                    slidesPerView: 1,
+                                    spaceBetween: 20,
+                                    initialSlide: 0
+                                },
+                                1024: {
+                                    slidesPerView: 2,
+                                    spaceBetween: 20,
+                                    initialSlide: 0
+                                },
+                                1280: {
+                                    slidesPerView: 3,
+                                    spaceBetween: 20,
+                                    allowTouchMove: false,
+                                    initialSlide: 1
+                                }
+                              }}
+                        >
+                            <SwiperSlide>
+                                <div className="w-[368px] h-[440px] mr-5">
+                                    <div className="h-[222px] bg-grey rounded-t-3xl overflow-hidden"><Image src={imgNovidades1} alt=""/></div>
+                                    <div className="h-[205px] bg-cold-white rounded-b-3xl p-10">
+                                        <span className="font-ubuntu text-grey block mb-6">19 Jun 2023</span>
+                                        <h3 className="text-black font-medium text-base mb-4">Cartão de combustível: o que é, como funciona  e qual o melhor?</h3>
+                                        <div className="-ml-[20px]">
+                                            <Button type="readMore" href="/">Leia mais</Button>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-                        <div className="w-[368px] h-[440px]">
-                            <div className="h-[235px] bg-grey rounded-t-3xl"></div>
-                            <div className="h-[205px] bg-cold-white rounded-b-3xl p-10">
-                                <span className="font-ubuntu text-grey block mb-6">19 Jun 2023</span>
-                                <h3 className="text-black font-medium text-base mb-4">Cartão de combustível: o que é, como funciona  e qual o melhor?</h3>
-                                <div className="-ml-[20px]">
-                                    <Button type="readMore" href="/">Leia mais</Button>
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <div className="w-[368px] h-[440px] mr-5">
+                                    <div className="h-[222px] bg-grey rounded-t-3xl overflow-hidden"><Image src={imgNovidades2} alt=""/></div>
+                                    <div className="h-[205px] bg-cold-white rounded-b-3xl p-10">
+                                        <span className="font-ubuntu text-grey block mb-6">19 Jun 2023</span>
+                                        <h3 className="text-black font-medium text-base mb-4">Cartão de combustível: o que é, como funciona  e qual o melhor?</h3>
+                                        <div className="-ml-[20px]">
+                                            <Button type="readMore" href="/">Leia mais</Button>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-                        <div className="w-[368px] h-[440px]">
-                            <div className="h-[235px] bg-grey rounded-t-3xl"></div>
-                            <div className="h-[205px] bg-cold-white rounded-b-3xl p-10">
-                                <span className="font-ubuntu text-grey block mb-6">19 Jun 2023</span>
-                                <h3 className="text-black font-medium text-base mb-4">Cartão de combustível: o que é, como funciona  e qual o melhor?</h3>
-                                <div className="-ml-[20px]">
-                                    <Button type="readMore" href="/">Leia mais</Button>
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <div className="w-[368px] h-[440px] mr-5">
+                                    <div className="h-[222px] bg-grey rounded-t-3xl overflow-hidden"><Image src={imgNovidades3} alt=""/></div>
+                                    <div className="h-[205px] bg-cold-white rounded-b-3xl p-10">
+                                        <span className="font-ubuntu text-grey block mb-6">19 Jun 2023</span>
+                                        <h3 className="text-black font-medium text-base mb-4">Cartão de combustível: o que é, como funciona  e qual o melhor?</h3>
+                                        <div className="-ml-[20px]">
+                                            <Button type="readMore" href="/">Leia mais</Button>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
+                            </SwiperSlide>
+                        </Swiper>
                     </div>
-                    <div className="flex justify-center">
+                    <div className="flex justify-center mt-10">
                         <Button type="learnMore" href="/">Veja todas as notícias</Button>
                     </div>
                 </div>
