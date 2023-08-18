8153d1252189e40968fa07f8188b05aeedd4cf56
gbrlcardozo <gabrielacardozo09@gmail.com>
2023-08-18 14:20:38 -0300

    primeiro bloco

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,32 +26,46 @@ const Home: NextPage = () => {
                         usando um aplicativo super simples e eficiente.
                     </p>
                     <div className="flex justify-between">
-                        <div className="relative flex">
-                            <svg width="704" height="500" viewBox="0 0 704 500" fill="none" xmlns="http://www.w3.org/2000/svg" className="">
+                        <div className="relative flex items-center w-[704px] h-[500px]">
+                      
+                            <div className="absolute right-0 top-0">
+                                <Image src={Hits1Img1} alt="" className=" top-0" width={500} height={500}></Image>
+                            </div>
+                                                        <svg width="314" height="500" viewBox="0 0 314 500" fill="none" xmlns="http://www.w3.org/2000/svg" className="absolute z-10 left-0 top-0">
                                 
-
-                                <image
-                                    xlinkHref={Hits1Img1.src}
-                                    className={`absolute top-0 opacity-100 transition-opacity duration-1000 '
-                                            }`}
-                                ></image>
-                                <path fill-rule="evenodd" clip-rule="evenodd" d="M64 0H-1779V500H64C202.071 500 314 388.071 314 250C314 111.929 202.071 0 64 0Z" fill="#C8C3FF"/>
-                                <path fill-rule="evenodd" clipRule="evenodd" d="M259 406.459C293.411 363.627 314 309.218 314 250C314 190.782 293.411 136.373 259 93.541C224.589 136.373 204 190.782 204 250C204 309.218 224.589 363.627 259 406.459Z" fill="#B0EFFF" />
+<path fill-rule="evenodd" clip-rule="evenodd" d="M64 0H-1779V500H64C202.071 500 314 388.071 314 250C314 111.929 202.071 0 64 0Z" fill="#C8C3FF"/>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M259 406.459C293.411 363.627 314 309.218 314 250C314 190.782 293.411 136.373 259 93.541C224.589 136.373 204 190.782 204 250C204 309.218 224.589 363.627 259 406.459Z" fill="#B0EFFF"/>                               
 
                             </svg>
+                            
+                       
+                                                        <div className="pr-6 text-left absolute right-16">
+                                <span className="inline-flex justify-center bg-light-violet px-4 py-2 rounded-full w-fit mb-2">Hit #01</span>
+                                <p className="text-white text-3xl max-w-[250px] mb-2">Quero controlar as despesas do negócio</p>
+                                <Button type="red" href="/combustivel">Eu quero</Button>
+                            </div>
                         </div>
-                        <div className="relative">
-                            <svg width="704" height="500" viewBox="0 0 704 500" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd" clipRule="evenodd" d="M640 500C501.929 500 390 388.071 390 250C390 111.929 501.929 0 640 0H890V250V500H640Z" fill="#DCF691" />
-                                <image
+                        <div className="relative flex items-center w-[704px] h-[500px]">
+                                                        <div className="absolute left-0 top-0   ">
+                                <Image src={Hits2Img2} alt="" className=" top-0" width={500} height={500}></Image>
+                            </div>
+                            <svg width="314" height="500" viewBox="0 0 314 500" fill="none" xmlns="http://www.w3.org/2000/svg" className="absolute z-10 right-0 top-0">
+                               
+                                {/* <image
                                     xlinkHref={Hits2Img2.src}
                                     className={`absolute top-0 opacity-100 transition-opacity duration-1000 '
                                             }`}
-                                ></image>
-                                <path fill-rule="evenodd" clipRule="evenodd" d="M445 406.459C410.589 363.627 390 309.218 390 250C390 190.782 410.589 136.373 445 93.541C479.411 136.373 500 190.782 500 250C500 309.218 479.411 363.627 445 406.459Z" fill="#FED0C8" />
+                                ></image> */}
+                                <path fill-rule="evenodd" clipRule="evenodd" d="M250 500C111.929 500 0 388.071 0 250C0 111.929 111.929 0 250 0H500V250V500H250Z" fill="#DCF691"/>
+<path fill-rule="evenodd" clipRule="evenodd" d="M55 406.459C20.5894 363.627 0 309.218 0 250C0 190.782 20.5894 136.373 55 93.541C89.4106 136.373 110 190.782 110 250C110 309.218 89.4106 363.627 55 406.459Z" fill="#FED0C8"/>
 
                             </svg>
 
+                                                                                    <div className="pr-6 text-right absolute left-16">
+                                <span className="inline-flex justify-center bg-[#DCF691]  px-4 py-2 rounded-full w-fit mb-2">Hit #02</span>
+                                <p className="text-white text-3xl max-w-[250px] mb-2">Quero controlar as despesas com combustível</p>
+                                <Button type="red" href="/combustivel">Eu quero</Button>
+                            </div>
                         </div>
                     </div>
                 </div>
