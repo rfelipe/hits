f8d0690f8722c10adaa98207abccff0ce4c63e50
dExBrunno <brunnoromanato@hotmail.com>
2023-08-21 16:19:51 -0300

    mobile pag home e ajustes textos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,12 @@ import { Phone, Clock, Note, Gas } from "../common/components/icons";
 import Button from "../common/components/Buttons";
 import Img2 from '../static/images/home/img1.png';
 import Img1 from '../static/images/home/img2.png';
+import Img3 from '../static/images/home/img3.png';
+import Img4 from '../static/images/home/img4.png';
 import Image from "next/image";
 import Hits1Img1 from '../static/images/img1_hits1.png';
+import Hits1Img1Mob from '../static/images/home/Group 47274.png';
+import Hits2Img2Mob from '../static/images/home/Group 47246.png';
 import Hits2Img2 from '../static/images/img1_hits2.png';
 import imgNovidades1 from '../static/images/home/novidades1.png'
 import imgNovidades2 from '../static/images/home/novidades2.png'
@@ -27,44 +31,42 @@ const Home: NextPage = () => {
                 <meta name="description" content=""></meta>
             </Head>
             <>
-                <div className="pt-32">
-                    <h2 className="text-5xl text-black font-light text-center mb-8">Quem conta com a gente acelera seu sucesso</h2>
+                <div className="pt-14 md:pt-32">
+                    <h2 className="text-5xl text-black font-light text-center md:max-w-4xl mx-auto mb-8">Descubra como gerenciar suas despesas diárias de forma eficaz!</h2>
                     <p className="text-grey md:text-black text-base md:text-lg font-light font-ubuntu mb-8 lg:w-[808px] mx-auto text-center">
-                        Ajudamos você a manter o controle dos gastos
-                        usando um aplicativo super simples e eficiente.
+                        Se você tem muito para conquistar, mas não tem tempo para perder, acelere seu sucesso com a Edenred Hits.
                     </p>
-                    <div className="flex justify-between flex-wrap gap-5">
+                    <div className="flex justify-between flex-wrap md:gap-5">
                         <div className="relative flex items-center w-[704px] h-[500px]">
-                      
-                            <div className="absolute right-0 top-0">
+                            <div className="hidden md:block absolute right-0 top-0">
                                 <Image src={Hits1Img1} alt="" className=" top-0" width={500} height={500}></Image>
                             </div>
-                                                        <svg width="314" height="500" viewBox="0 0 314 500" fill="none" xmlns="http://www.w3.org/2000/svg" className="absolute z-10 left-0 top-0">
-                                
-<path fill-rule="evenodd" clip-rule="evenodd" d="M64 0H-1779V500H64C202.071 500 314 388.071 314 250C314 111.929 202.071 0 64 0Z" fill="#C8C3FF"/>
-<path fill-rule="evenodd" clip-rule="evenodd" d="M259 406.459C293.411 363.627 314 309.218 314 250C314 190.782 293.411 136.373 259 93.541C224.589 136.373 204 190.782 204 250C204 309.218 224.589 363.627 259 406.459Z" fill="#B0EFFF"/>                               
+                            <div className="md:hidden absolute left-0 top-0">
+                                <Image src={Hits1Img1Mob} alt="" className=" top-0" width={400} height={400}></Image>
+                            </div>
 
-                            </svg>
-                            
-                       
-                                                        <div className="pr-6 text-left absolute right-16">
+                            <svg width="314" height="500" viewBox="0 0 314 500" fill="none" xmlns="http://www.w3.org/2000/svg" className="hidden md:block absolute z-10 left-0 top-0">
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M64 0H-1779V500H64C202.071 500 314 388.071 314 250C314 111.929 202.071 0 64 0Z" fill="#C8C3FF"/>
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M259 406.459C293.411 363.627 314 309.218 314 250C314 190.782 293.411 136.373 259 93.541C224.589 136.373 204 190.782 204 250C204 309.218 224.589 363.627 259 406.459Z" fill="#B0EFFF"/>                               
+                            </svg>  
+                            <div className="md:pr-6 text-center md:text-left absolute max-md:left-24 max-md:bottom-20 md:right-16">
                                 <span className="inline-flex justify-center bg-light-violet px-4 py-2 rounded-full w-fit mb-2">Hit #01</span>
                                 <p className="text-white text-3xl max-w-[250px] mb-2">Quero controlar as despesas do negócio</p>
                                 <Button type="red" href="/combustivel">Eu quero</Button>
                             </div>
                         </div>
                         <div className="relative flex items-center ml-auto w-[704px] h-[500px]">
-                                                        <div className="absolute left-0 top-0   ">
+                            <div className="hidden md:block absolute left-0 top-0   ">
                                 <Image src={Hits2Img2} alt="" className=" top-0" width={500} height={500}></Image>
                             </div>
-                            <svg width="314" height="500" viewBox="0 0 314 500" fill="none" xmlns="http://www.w3.org/2000/svg" className="absolute z-10 right-0 top-0">
-                            
+                            <div className="md:hidden absolute right-0 top-0   ">
+                                <Image src={Hits2Img2Mob} alt="" className=" top-0" width={400} height={400}></Image>
+                            </div>
+                            <svg width="314" height="500" viewBox="0 0 314 500" fill="none" xmlns="http://www.w3.org/2000/svg" className="hidden md:block absolute z-10 right-0 top-0">
                                 <path fill-rule="evenodd" clipRule="evenodd" d="M250 500C111.929 500 0 388.071 0 250C0 111.929 111.929 0 250 0H500V250V500H250Z" fill="#DCF691"/>
-<path fill-rule="evenodd" clipRule="evenodd" d="M55 406.459C20.5894 363.627 0 309.218 0 250C0 190.782 20.5894 136.373 55 93.541C89.4106 136.373 110 190.782 110 250C110 309.218 89.4106 363.627 55 406.459Z" fill="#FED0C8"/>
-
+                                <path fill-rule="evenodd" clipRule="evenodd" d="M55 406.459C20.5894 363.627 0 309.218 0 250C0 190.782 20.5894 136.373 55 93.541C89.4106 136.373 110 190.782 110 250C110 309.218 89.4106 363.627 55 406.459Z" fill="#FED0C8"/>
                             </svg>
-
-                                                                                    <div className="pr-6 text-right absolute left-16">
+                            <div className="pr-6 md:text-right text-center max-md:right-16 max-md:bottom-20  absolute md:left-16">
                                 <span className="inline-flex justify-center bg-[#DCF691]  px-4 py-2 rounded-full w-fit mb-2">Hit #02</span>
                                 <p className="text-white text-3xl max-w-[250px] mb-2">Quero controlar as despesas com combustível</p>
                                 <Button type="red" href="/combustivel">Eu quero</Button>
@@ -72,13 +74,12 @@ const Home: NextPage = () => {
                         </div>
                     </div>
                 </div>
-                <div className="pt-32"> {/* DONE MOBILE AND DESK */}
-                    <h2 className="text-5xl text-black font-light text-center mb-8">Veja como é fácil</h2>
-                    <p className="text-grey md:text-black text-base md:text-lg font-light font-ubuntu mb-8 lg:w-[808px] mx-auto text-center">
-                        Se você tem um <strong>comércio ou é atacadista, prestador de serviços, construtura ou pequena indústria</strong>, você precisa testar essa solução pra manter suas contas em dia, agilizar processos
-                        e acelerar o crescimento do seu negócio.
+                <div className="md:pt-32"> {/* DONE MOBILE AND DESK */}
+                    <h2 className="text-5xl text-black font-light text-center mb-8">Experimente a facilidade em primeira mão!</h2>
+                    <p className="text-grey md:text-black text-base md:text-lg font-light font-ubuntu px-6 mb-8 lg:w-[808px] mx-auto text-center">
+                        Se você tem um <strong>comércio ou é atacadista, prestador de serviços, construtora ou pequena indústria</strong>, você precisa testar essa solução pra manter suas contas em dia, agilizar processos e acelerar o crescimento do negócio
                     </p>
-                    <div className="pt-16">
+                    <div className="pt-7 md:pt-16">
                         <div className="lg:max-w-[90%] ml-auto mr-auto">
                         <Swiper slidesPerView={4} initialSlide={1}
                             breakpoints={{
@@ -101,7 +102,7 @@ const Home: NextPage = () => {
                                         <Phone />
                                     </div>
                                     <h3 className="text-black font-medium text-2xl mb-2">Controle</h3>
-                                    <p className="w-[188px] text-center mx-auto">Controle suas despesas em tempo real</p>
+                                    <p className="w-[188px] text-center mx-auto">Aplicativo e plataforma para gestão sem custo adicional.</p>
                                 </div>
                             </SwiperSlide>
                             <SwiperSlide className="flex justify-center">
@@ -109,8 +110,8 @@ const Home: NextPage = () => {
                                     <div className="mb-4">
                                         <Gas />
                                     </div>
-                                    <h3 className="text-black font-medium text-2xl mb-2">Controle</h3>
-                                    <p className="w-[188px] text-center mx-auto">Controle suas despesas em tempo real</p>
+                                    {/* <h3 className="text-black font-medium text-2xl mb-2">Controle</h3> */}
+                                    <p className="w-[188px] text-center mx-auto">Use onde precisar com uma ampla rede parceira.</p>
                                 </div>
                             </SwiperSlide>
                             <SwiperSlide className="flex justify-center">
@@ -118,8 +119,8 @@ const Home: NextPage = () => {
                                     <div className="mb-4">
                                         <Clock />
                                     </div>
-                                    <h3 className="text-black font-medium text-2xl mb-2">Controle</h3>
-                                    <p className="w-[188px] text-center mx-auto">Controle suas despesas em tempo real</p>
+                                    {/* <h3 className="text-black font-medium text-2xl mb-2">Controle</h3> */}
+                                    <p className="w-[188px] text-center mx-auto">Tempo para focar no crescimento do seu negócio.</p>
                                 </div>
                             </SwiperSlide>
                             <SwiperSlide className="flex justify-center">
@@ -127,15 +128,15 @@ const Home: NextPage = () => {
                                     <div className="mb-4">
                                         <Note />
                                     </div>
-                                    <h3 className="text-black font-medium text-2xl mb-2">Controle</h3>
-                                    <p className="w-[188px] text-center mx-auto">Controle suas despesas em tempo real</p>
+                                    {/* <h3 className="text-black font-medium text-2xl mb-2">Controle</h3> */}
+                                    <p className="w-[188px] text-center mx-auto">Visibilidade e controle em todos os gastos.</p>
                                 </div>
                             </SwiperSlide>
                         </Swiper>
                         </div>
                     </div>
                 </div>
-                <div className="pt-32"> {/* DONE MOBILE AND DESK */}
+                <div className="pt-14 md:pt-32"> {/* DONE MOBILE AND DESK */}
                     <h2 className="text-5xl text-black font-light text-center mb-16">Veja as novidades</h2>
                     <div className="lg:max-w-[90%] ml-auto mr-auto mt-12">
                         <Swiper slidesPerView={3} initialSlide={2} spaceBetween={30}
@@ -197,14 +198,17 @@ const Home: NextPage = () => {
                         </Swiper>
                     </div>
                     <div className="flex justify-center mt-10">
-                        <Button type="learnMore" href="/">Veja todas as notícias</Button>
+                        <Button type="learnMore" href="/">Confira todas as notícias</Button>
                     </div>
                 </div>
-                <div className="pt-32">
-                    <div className="flex">
+                <div className="pt-14 md:pt-32 mb-14">
+                    <div className="flex max-xl:flex-col-reverse max-xl:flex-wrap mb-10">
                         <div className="flex">
-                            <div className="relative z-10 shrink-0" >
-                                <Image alt="" src={Img1} />
+                            <div className="relative z-10 shrink-0 hidden md:block" >
+                                <Image alt="" src={Img1} className=""/>
+                            </div>
+                            <div className="relative z-10 shrink-0 md:hidden" >
+                                <Image alt="" src={Img4} className=""/>
                             </div>
                             <div className="relative flex items-center justify-center bg-cold-white w-[309px] h-[309px] rounded-[100%] ml-[-66px] z-0 ">
                                 <div className="pl-6">
@@ -214,7 +218,7 @@ const Home: NextPage = () => {
                                 </div>
                             </div>
                         </div>
-                        <h2 className="text-black text-6xl ml-auto font-light w-[500px]">Vem pro lado sucesso da força</h2>
+                        <h2 className="text-black text-6xl ml-auto my-auto font-light max-xl:max-w-4xl max-xl:text-center max-xl:mb-10 xl:w-[500px] mx-auto">Vem pro lado do sucesso, compre o seu Hit online!</h2>
                     </div>
                     <div className="flex justify-end">
                         <div className="relative flex items-center justify-center bg-cold-white w-[309px] h-[309px] rounded-[100%] mr-[-66px] z-0 ">
@@ -224,7 +228,12 @@ const Home: NextPage = () => {
                                 <Button type="red" href="/combustivel">Eu quero</Button>
                             </div>
                         </div>
-                        <Image alt="" src={Img2} className="relative z-0" />
+                        <div className="hidden md:block">
+                            <Image alt="" src={Img2} className=" relative z-0" />
+                        </div>
+                        <div className="md:hidden">
+                            <Image alt="" src={Img3} className=" relative z-0" />
+                        </div>
                     </div>
                 </div>
             </>
