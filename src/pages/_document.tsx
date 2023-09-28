8e6ca53080a9ccbdb09a2ee43e37b59a119690a9
Paulo Paixão <you@example.com>
2023-09-28 14:20:20 -0300

    altera GTM

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -27,14 +27,14 @@ const Document = () => (
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-MVR5MFF');
+          })(window,document,'script','dataLayer','GTM-5RGV4Z6');
           `
         }}
       />          
     </Head>
     <body>
       <noscript>
-      <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MVR5MFF" height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe>
+      <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5RGV4Z6" height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe>
       </noscript>
       <Main />
       <NextScript />      
