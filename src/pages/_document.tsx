52efce65ec24e585d12f92ca69938e48db92adc8
Paulo Paixão <you@example.com>
2023-10-09 15:32:14 -0300

    ajute gtm

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 import Script from "next/script";
 
 const Document = () => (
-  <Html lang="pt-br" className="overflow-x-hidden">
+  <Html lang="pt-br">
     <Head>
       <link
         rel="shortcut icon"
@@ -17,10 +17,11 @@ const Document = () => (
         defer
         src="https://cdn.cookielaw.org/consent/daaec1ff-c365-4c92-85ec-6b69eea72558/otSDKStub.js"
         type="text/javascript"
+        charSet="UTF-8"
         data-domain-script="daaec1ff-c365-4c92-85ec-6b69eea72558"
       ></script>
       <script type="text/javascript">function OptanonWrapper() {}</script>
-      <Script
+      <script
         id="gtm"
         dangerouslySetInnerHTML={{
           __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -28,7 +29,7 @@ const Document = () => (
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','GTM-5RGV4Z6');
-          `
+          `,
         }}
       />          
     </Head>
