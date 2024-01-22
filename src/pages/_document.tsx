6d5d50d0317d590c1f9cc7ff9ae50ae1137810e9
rfelipe <f.questao@gmail.com>
2024-01-22 09:50:02 -0300

    inserirr codigo chatbot

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -31,7 +31,114 @@ const Document = () => (
           })(window,document,'script','dataLayer','GTM-5RGV4Z6');
           `,
         }}
-      />          
+      />  
+            <script src="https://cdn.data2.media/file/powercord-0.0.1.min.js"></script>
+      <script src="https://cdn.data2.media/file/powercord-0.0.1.min.js" dangerouslySetInnerHTML={{
+          __html: 
+        `
+        const urlParams = new URLSearchParams(window.location.search);
+        let chatId = urlParams.get("chat_id");
+        
+        const queryParams = {
+          "d2_pl-prv": true,
+        };
+        if (chatId) {
+          queryParams["chat_id"] = chatId;
+        }
+        
+        const queryParamString = new URLSearchParams(queryParams).toString();
+        
+        const chat = new PowerCord(
+          \`https://chatroom.edenred-ticketlog.data2.app/?\${queryParamString}\`,
+          {
+            onInitialized: function setupEmitEvents(chat) {
+              let windowState = {};
+              
+              function emitWindowState() {
+                const nextWindowState = {
+                  pathname: window.location.pathname,
+                  scroll: window.scrollY,
+                  isCheckout: window.location.hostname.includes("loja."),
+                  windowWidth: window.innerWidth,
+                };
+                
+                const hasChanges = Object.keys(nextWindowState)
+                  .map((key) => nextWindowState[key] !== windowState[key])
+                  .filter(Boolean).length > 0;
+                
+                if (hasChanges) {
+                  windowState = nextWindowState;
+                  chat.emit("window-state", {
+                    pathname: window.location.pathname,
+                    scroll: window.scrollY,
+                    isCheckout: window.location.hostname.includes("loja."),
+                    windowWidth: window.innerWidth,
+                  });
+                }
+              }
+              
+              emitWindowState();
+              setInterval(emitWindowState, 500);
+            },
+          }
+        );
+        
+        chat.createInstance({
+          style: {
+            width: "150px",
+            height: "150px",
+            bottom: "0px",
+          },
+        });
+        
+        chat.addEventListener("navigation", (data) => {
+          if (data.path) {
+            let el = document.querySelector(\`[href="/\${data.path}"]\`);
+            let url = url.getAttribute("href");
+            if (url.indexOf("?") !== -1) {
+              el.setAttribute(
+                "href",
+                el.getAttribute("href") + "&chat_id=" + window.chat_id
+              );
+            } else {
+              el.setAttribute(
+                "href",
+                el.getAttribute("href") + "?chat_id=" + window.chat_id
+              );
+            }
+            return el.click();
+          }
+          if (data.url) {
+            if (data.url.indexOf("?") !== -1) {
+              return window.open(data.url + "&chat_id=" + window.chat_id);
+            } else {
+              return window.open(data.url + "?chat_id=" + window.chat_id);
+            }
+          }
+        });
+        
+        chat.addEventListener("click", (data) => {
+          if (data.selector) {
+            document.querySelector(data.selector).click();
+          }
+        });
+        
+        chat.addEventListener("scroll", (data) => {
+          if (data.selector) {
+            document
+              .querySelector(data.selector)
+              .scrollIntoView({ behavior: "smooth", block: "start" });
+          }
+        });
+        
+        chat.addEventListener("set-chat-id", (data) => {
+          window.chat_id = data.chatId;
+        });
+        
+        chat.addEventListener("change-style", (style) => {
+          if (typeof style === "object") chat.applyInstanceStyle(style);
+        });
+      `}}></script>        
     </Head>
     <body>
       <noscript>
