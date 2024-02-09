2ea03013ae86d12cbad523b243cda21e9e5bc253
rfelipe <f.questao@gmail.com>
2024-02-09 15:25:04 -0300

    remover texto PrivacyPolicy da modal

--- src/common/components/Popup/index.tsx
+++ src/common/components/Popup/index.tsx
@@ -89,24 +89,6 @@ const Popup = () => {
                         <Image src={arrow} alt="Seta" />
                     </div>
                 </button>
-                {/* <p style={{
-                    fontSize:'10px',
-                    fontWeight:'300',
-                    lineHeight:'13.24px',
-                    textAlign:'center',
-                    width:'458px',
-                    height:'26px',
-                    marginTop: '30px',
-                }}>
-                Você concorda que os dados informados podem ser utilizados para o envio de ofertas de produtos
-                    e serviços, de acordo com nossa <a 
-                                                        href="https://www.edenredpay.com.br/politica-privacidade/"
-                                                        target="_blank"
-                                                        rel="noreferrer"
-                                                        style={{fontWeight:'500',lineHeight:'13.35px'}}>
-                                                            Política de Privacidade da Edenred Pay.
-                                                    </a>
-                </p> */}
                 <PrivacyPolicy />
             </div>
           )}
